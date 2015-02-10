720cb1a746ba8b9bbc2a21d283069f7a2560e8d0
JamesMcGuigan <james.mcguigan@gmail.com>
2015-02-10 01:28:56 +0000

    Speed up image slideshow

--- app/public/angular/routes.js
+++
@@ -1,5 +0,0 @@
-angular.module('infographicApp.routes',['ngRoute']).config(["$routeProvider",
-    function($routeProvider) {
-	    $routeProvider.when("/", { templateUrl: "html/list.html", controller: "ListController" });
-    }
-]);
